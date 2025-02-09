989ef183433d079dc52981a999ef481c9747af3a
Yash Pathik <yashpathik.official@gmail.com>
2025-02-10 00:01:01 +0530

    test commit

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,18 @@
-import { Jost } from 'next/font/google';
+import { Jost } from "next/font/google";
 
-import './globals.css';
+import "./globals.css";
 
-import Header from '@/components/Header/header';
-import Footer from '@/components/Footer/footer';
+import Header from "@/components/Header/header";
+import Footer from "@/components/Footer/footer";
 
 const jost = Jost({
-    subsets: ['latin'],
-    display: 'swap',
+    subsets: ["latin"],
+    display: "swap",
 });
 
 export const metadata = {
-    title: 'Portfolio | Yash Pathik',
-    description: 'Web Developer Portfolio of Yash Pathik',
+    title: "Portfolio | Yash Pathik",
+    description: "A Web Developer Portfolio of Yash Pathik",
 };
 
 export default function RootLayout({ children }) {
